db9b3404b2817745cedc6cbed580a7b1e5ce6d97
Minshuai Jiang <jiangmi@tcd.ie>
2024-02-22 20:56:25 +0000

    change the thresholds

--- matlab/match_img.m
+++ matlab/match_img.m
@@ -1,26 +1,3 @@
-% function [F_rgb, B_rgb, U_rgb] = match_img(rgb, trimap)
-%     F_threshold = 255 * 0.9;
-%     B_threshold = 255 * 0.0;
-%     width = size(rgb,1);
-%     heigth = size(rgb,2);
-%     for w = 1 : width
-%         for h = 1 : heigth
-%             if trimap(w, h) >= F_threshold
-%                 F_rgb(w, h, 1) = rgb(w, h, 1);
-%                 F_rgb(w, h, 2) = rgb(w, h, 2);
-%                 F_rgb(w, h, 3) = rgb(w, h, 3);
-%             elseif trimap(w, h) <= B_threshold
-%                 B_rgb(w, h, 1) = rgb(w, h, 1);
-%                 B_rgb(w, h, 2) = rgb(w, h, 2);
-%                 B_rgb(w, h, 3) = rgb(w, h, 3);
-%             else
-%                 U_rgb(w, h, 1) = rgb(w, h, 1);
-%                 U_rgb(w, h, 2) = rgb(w, h, 2);
-%                 U_rgb(w, h, 3) = rgb(w, h, 3);
-%             end
-%         end
-%     end
-
 function [F_rgb, B_rgb, U_rgb] = match_img(rgb, trimap)
     % Initialize RGB output matrices with zeros
     F_rgb = zeros(size(rgb), 'like', rgb);
@@ -28,8 +5,8 @@ function [F_rgb, B_rgb, U_rgb] = match_img(rgb, trimap)
     U_rgb = zeros(size(rgb), 'like', rgb);
 
     % Define thresholds
-    F_threshold = 255 * 0.9;
-    B_threshold = 255 * 0.0;
+    F_threshold = 255 * 0.95;
+    B_threshold = 255 * 0.05;
     
     % Find foreground, background, and unknown pixels using logical indexing
     F_mask = trimap >= F_threshold;
