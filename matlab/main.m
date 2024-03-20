37747d08b4db259255de46cfd771b5a074f8de9c
Minshuai Jiang <jiangmi@tcd.ie>
2024-03-20 12:24:26 +0000

    matlab images change

--- matlab/main.m
+++ matlab/main.m
@@ -2,13 +2,13 @@ clc;
 clear;
 close all;
 
-rgb_path = 'test/GT01_image.png';
-trimap_path = 'test/GT01_trimap.png';
+rgb_path = 'test/GT08_image.png';
+trimap_path = 'test/GT08_trimap.png';
 rgb = imread(rgb_path);
 trimap = imread(trimap_path);
 %%
 tic;
-alpha = calculate_alpha(rgb_path, trimap_path, 8, 30);
+alpha = calculate_alpha(rgb_path, trimap_path, 8, 10);
 toc;
 alpha = imbinarize(alpha);
 %%
@@ -20,7 +20,7 @@ B(:,:,1) = double(1 - alpha) .* double(rgb(:,:,1));
 B(:,:,2) = double(1 - alpha) .* double(rgb(:,:,2));
 B(:,:,3) = double(1 - alpha) .* double(rgb(:,:,3));
 % Display the result
-gt = imread("./test/GT01.png");
+gt = imread("./test/GT08.png");
 figure(1);
 
 subplot(3,2,1);
